1f698888f2117b0dfc4513a767466ea7e6959fb0
StefanJKruger <stefankruger42@yahoo.com>
2025-01-15 21:14:03 +0200

    final version LandingPage.js

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './LandingPage.css';
+import Chips from '../ProductPage/simbachips.jpg'
 
+//Landing page function to always appear first upon arrival
 const LandingPage = () => {
   return (
     <Container className="landing-container">
       <Row className="align-items-center">
         <Col md={6}>
-          <h1>Welcome to E-Shop</h1>
+          <h1>Welcome to Mzanzi Traders</h1>
           <p className="lead">
-            Discover our amazing products at great prices!
+            Discover our amazing local products. Proudly South African!
           </p>
           <Button as={Link} to="/products" variant="primary" size="lg">
             Shop Now
@@ -18,7 +20,7 @@ const LandingPage = () => {
         </Col>
         <Col md={6}>
           <img
-            src="/api/placeholder/500/300"
+            src={Chips}
             alt="Featured products"
             className="img-fluid"
           />
